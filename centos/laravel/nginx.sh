2598ca7585f0e1bafba3926f48f255c1dbf5acaf
ojiepermana <me@ojiepermana.com>
2023-08-05 15:01:17 +0700

    Update nginx.sh

--- centos/laravel/nginx.sh
+++ centos/laravel/nginx.sh
@@ -1,1 +1,5 @@
-#
+sudo dnf update
+sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
+sudo dnf module enable php:remi-8.2 -y
+sudo dnf install nginx 
+sudo dnf install -y php php-fpm
