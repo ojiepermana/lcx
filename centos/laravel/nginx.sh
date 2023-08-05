2d192df6e49abec86f7ca78df376b7c15ab11304
ojiepermana <me@ojiepermana.com>
2023-08-05 15:04:10 +0700

    Delete nginx.sh

--- centos/laravel/nginx.sh
+++
@@ -1,5 +0,0 @@
-sudo dnf update
-sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
-sudo dnf module enable php:remi-8.2 -y
-sudo dnf install nginx 
-sudo dnf install -y php php-fpm
