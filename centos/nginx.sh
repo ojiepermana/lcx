63cfae375786c9d8c7ce9b290f65776788bef9f9
ojiepermana <me@ojiepermana.com>
2023-08-06 14:18:30 +0700

    Update nginx.sh

--- centos/nginx.sh
+++ centos/nginx.sh
@@ -3,3 +3,12 @@ sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 sudo dnf module enable php:remi-8.2 -y
 sudo dnf install nginx 
 sudo dnf install -y php php-fpm
+
+sudo dnf install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+systemctl status firewalld
+
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
