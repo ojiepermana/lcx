70b02a8a36f04c1d5896346391758372d1cdea9f
ojiepermana <me@ojiepermana.com>
2023-08-06 15:10:04 +0700

    Update octane-mysql.sh

--- centos/laravel/octane-mysql.sh
+++ centos/laravel/octane-mysql.sh
@@ -2,7 +2,8 @@ sudo dnf update -y
 sudo dnf install wget curl zip unzip git -y
 sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 sudo dnf module enable php:remi-8.2 -y
-sudo dnf install -y php-cli
+sudo dnf install -y php-cli php-mysqlnd
+
 
 #composer
 wget https://getcomposer.org/installer -O composer-installer.php
