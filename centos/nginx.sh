1a52bffeeb4e10832cf7bc0227f23652a2d9e2c0
ojiepermana <me@ojiepermana.com>
2023-08-10 12:23:48 +0700

    Update nginx.sh

--- centos/nginx.sh
+++ centos/nginx.sh
@@ -1,6 +1,10 @@
 sudo dnf update -y
 sudo dnf install nginx nano wget curl zip unzip git epel-release ncurses -y
 
+sudo dnf install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
 #php php-fpm
 sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 sudo dnf module enable php:remi-8.2 -y
