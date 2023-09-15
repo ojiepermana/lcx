849bb59b1da20751878e512e68e70ba0c39aec3b
ojiepermana <me@ojiepermana.com>
2023-09-15 09:46:48 +0700

    Update phpmariadb.sh

--- centos/phpmariadb.sh
+++ centos/phpmariadb.sh
@@ -7,7 +7,7 @@ sudo systemctl enable nginx
 
 #php php-fpm
 sudo dnf install -y https://rpms.remirepo.net/enterprise/remi-release-9.rpm
-sudo dnf module enable php:remi-8.0 -y
+sudo dnf module enable php:remi-8.2 -y
 sudo dnf install -y php php-fpm
 
 sudo systemctl enable php-fpm
