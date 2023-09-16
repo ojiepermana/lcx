891a4292c9ba5c7863aa1ce58f07be2963408ced
ojiepermana <me@ojiepermana.com>
2023-09-15 21:10:01 -0700

    updates

--- npm/distro/debian.sh
+++ npm/distro/debian.sh
@@ -103,7 +103,7 @@ log "Installing nodejs"
 runcmd apt update
 runcmd apt install -y ca-certificates curl gnupg
 runcmd mkdir -p /etc/apt/keyrings
-runcmd curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+runcmd curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 NODE_MAJOR=18
 runcmd echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
