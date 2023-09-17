5dd1d9d6af51e4fddf58f69fc70d1231a5eb52ef
ojiepermana <me@ojiepermana.com>
2023-09-16 17:26:19 -0700

    qw

--- npm/distro/debian.sh
+++ npm/distro/debian.sh
@@ -110,6 +110,7 @@ runcmd mkdir -p /etc/apt/keyrings
 runcmd curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+runcmd npm install --global yarn
 
 # Get latest version information for nginx-proxy-manager
 log "Checking for latest NPM release"
